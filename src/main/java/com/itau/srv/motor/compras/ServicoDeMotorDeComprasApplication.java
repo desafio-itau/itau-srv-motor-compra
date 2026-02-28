593589cf787ae03e594054aba7d7d395611ba593
Leonardo Lima <leo2342005@gmail.com>
2026-02-28 18:54:20 -0300

    feat: adicionando configuração incial do projeto.

---
+++ src/main/java/com/itau/srv/motor/compras/ServicoDeMotorDeComprasApplication.java
@@ -0,0 +1,13 @@
+package com.itau.srv.motor.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeMotorDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeMotorDeComprasApplication.class, args);
+	}
+
+}
